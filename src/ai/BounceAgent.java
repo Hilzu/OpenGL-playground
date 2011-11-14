b2fb950b3dedc2f26a8d2ac67e60ab6044c2859d
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2011-11-15 01:54:28 +0200

    Made to use getScale() and increased speed

--- src/ai/BounceAgent.java
+++ src/ai/BounceAgent.java
@@ -7,7 +7,7 @@ import org.lwjgl.util.vector.Vector2f;
 public class BounceAgent {
     
     private Square square;
-    private final float speed = 0.001f;
+    private final float speed = 0.003f;
     private float xSpeed = speed;
     private float ySpeed = speed;
     
@@ -17,16 +17,17 @@ public class BounceAgent {
     
     public void tick() {
         Vector2f loc = square.getLocation();
+        Vector2f scale = square.getScale();
         
-        if (loc.x > 1 && Math.signum(xSpeed) != -1.0f) {
+        if ((loc.x + scale.x)> 1 && Math.signum(xSpeed) != -1.0f) {
             xSpeed = -xSpeed;
-        } else if (loc.x < -1 && Math.signum(xSpeed) != 1.0f) {
+        } else if ((loc.x - scale.x) < -1 && Math.signum(xSpeed) != 1.0f) {
             xSpeed = -xSpeed;
         }
         
-        if (loc.y > 1 && Math.signum(ySpeed) != -1.0f) {
+        if ((loc.y + scale.y) > 1 && Math.signum(ySpeed) != -1.0f) {
             ySpeed = -ySpeed;
-        } else if (loc.y < -1 && Math.signum(ySpeed) != 1.0f) {
+        } else if ((loc.y - scale.y) < -1 && Math.signum(ySpeed) != 1.0f) {
             ySpeed = -ySpeed;
         }
         
